307f735492cc773afe13abbebd0a69d6c5f3d911
Warren <warrens0924@gmail.com>
2021-06-14 13:14:51 -0700

    added

---
+++ NYPD/nypd.sql
@@ -0,0 +1,13 @@
+create table nypd(
+	"arrest_date" varchar, 
+	"police_description" varchar,
+	"offense_description" varchar,
+	"boro_code" varchar, 
+	"arresting_precinct" varchar,
+	"age_range" varchar,
+	"sex" varchar,
+	"race" varchar
+
+);
+
+select * from nypd;
